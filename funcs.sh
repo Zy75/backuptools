072eedc1741170b4bf3a841fb129ed9ea814c3bd
Your Name <you@example.com>
2018-09-14 19:35:38 -0400

    add restore

---
+++ funcs.sh
@@ -0,0 +1,48 @@
+
+function check_bkup_label() {
+
+  info_bkup_part=`lsblk -f | grep $1 | grep bkup`
+
+  length=`echo ${#info_bkup_part}`
+
+  if [ $length -eq 0 ] ; then
+    echo "ERROR: No bkup label set on bkup partition."
+    exit 1
+  else
+    echo -e "${GREEN}OK bkup LABEL${NC}: /dev/$1 "
+  fi
+
+}
+
+function check_partition_exist() {
+
+  if [ ! -e "/dev/$1" ] ; then
+    echo "ERROR: /dev/$1 does not exist."
+    exit 1
+  else
+    echo -e "${GREEN}OK${NC} ( check_partition_exist /dev/$1 )"
+  fi
+}
+
+# GOOD: sda1 sdb3
+# BAD: sdc sda ( Disk name is dangerous. )
+function check_partition_postfix_number(){
+  re='^[0-9]$'
+  if ! [[ ${1: -1} =~ $re ]] ; then
+    echo "ERROR: $1 must be prefixed by numbers."
+    exit 1
+  else
+    echo -e "${GREEN}OK${NC} ( check_partition_postfix_number /dev/$1 )"
+  fi
+}
+
+function check_protected() {
+  for protected in $protected_part
+  do
+    if [ $protected = $1 ] ; then
+      echo "ERROR: /dev/$1 is protected in conf file."
+      exit 1
+    fi
+  done
+}
+
---
+++ restore.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# run as sudo or root
+
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+. ./funcs.sh
+
+echo ""
+
+source ./dump_restore.conf
+
+check_partition_exist $backup_to
+check_bkup_label $backup_to
+check_partition_postfix_number $backup_to
+
+echo ""
+
+echo "Disabling SELinux..."
+setenforce 0
+
+echo ""
+
+mkdir /bkup > /dev/null 2>&1
+umount /bkup > /dev/null 2>&1
+
+mount "/dev/$backup_to" /bkup
+
+mkdir /restore > /dev/null 2>&1
+umount /restore > /dev/null 2>&1
+
+for restore_part in $backup_from_sdb
+do
+  check_partition_exist $restore_part
+  check_partition_postfix_number $restore_part
+  check_protected $restore_part
+
+  dev_restore="/dev/$restore_part"
+
+  echo -e "${GREEN}Restoring $dev_restore...${NC}"
+
+  mkfs -t ext4 $dev_restore > /dev/null 2>&1
+#  ret=$?
+
+#  if [ ! $ret == 0 ] ; then
+#    echo "FAILED: exit."
+#    exit 1
+#  fi
+
+  mount $dev_restore /restore
+  
+  cd /restore
+
+  restore -r -f "/bkup/$restore_from_dir/$restore_part.dump"
+
+  cd ~
+
+  umount /restore
+
+  echo -e "${GREEN}Restoring finished. $dev_restore...${NC}"
+  echo -e "${GREEN}PRESS ENTER.${NC}"
+  read
+done
+
